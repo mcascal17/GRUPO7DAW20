8032c3b1cc2e30451142f5af37c5741ca5a905bd
josejam <joseam.jam.79@gmail.com>
2020-11-18 21:40:29 +0100

    Primer commit Marin [Config.php/Controller.php/index.php]

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos72020";
+     static public $mvc_bd_usuario  = "myanta2200";
+     static public $mvc_bd_clave    = "8dxCQBlK";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
